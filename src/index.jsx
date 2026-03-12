49e147210a401cc480ac20fa8c615e7b5668194a
Jainil Prajapati <github-jainil@1209.in>
2026-03-12 13:13:33 +0530

    refactor: Migrate font loading from Google Fonts `@import` to `@fontsource-variable` packages and remove `dist/index.html`.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource-variable/manrope";
+import "@fontsource-variable/outfit";
+import "@fontsource-variable/playfair-display";
 import "@/index.css";
 import App from "@/App";
 
